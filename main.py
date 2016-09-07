9594e934aa0133747f08e74e9f61afbf30692489
Tuckner <tuckner329@gmail.com>
2016-09-07 14:47:42 -0500

    Added text field values

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def query():
         	zid2 = request.args.get("zid2")
         	zid3 = request.args.get("zid3")
         	zroute, zembed = zmaps.genroute(zid1, zid2, zid3)
-        	return render_template('index.html', zroute=zroute, zembed=zembed)
+        	return render_template('index.html', zid1=zid1, zid2=zid2, zid3=zid3, zroute=zroute, zembed=zembed)
 	
 	except:
 		oops = "Error"
