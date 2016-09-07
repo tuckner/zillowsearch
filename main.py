6db9e12091e1f444d58b61c35e705a11ef86d475
Tuckner <tuckner329@gmail.com>
2016-09-07 13:52:55 -0500

    Added error checking

--- main.py
+++ main.py
@@ -2,31 +2,24 @@ import zmaps
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def hello():
 
 	return render_template('index.html')
 
-@app.route("/", methods=['POST'])
-def search():
-
-	startloc = request.form.get("startloc")
-	endloc = request.form.get("endloc")
-	zid1 = request.form.get("zid1")
-	zid2 = request.form.get("zid2")
-	zid3 = request.form.get("zid3")
-	zroute, zembed = zmaps.genroute(zid1, zid2, zid3)
-	return render_template('index.html', zroute=zroute, zembed = zembed)
-
 @app.route("/query")
 def query():
 	
-        zid1 = request.args.get("zid1")
-        zid2 = request.args.get("zid2")
-        zid3 = request.args.get("zid3")
-        zroute, zembed = zmaps.genroute(zid1, zid2, zid3)
-        return render_template('index.html', zroute=zroute, zembed = zembed)
-
+	try:
+        	zid1 = request.args.get("zid1")
+        	zid2 = request.args.get("zid2")
+        	zid3 = request.args.get("zid3")
+        	zroute, zembed = zmaps.genroute(zid1, zid2, zid3)
+        	return render_template('index.html', zroute=zroute, zembed=zembed)
+	
+	except:
+		oops = "Error"
+		return render_template('index.html', oops=oops)
 
 
 if __name__ == "__main__":
